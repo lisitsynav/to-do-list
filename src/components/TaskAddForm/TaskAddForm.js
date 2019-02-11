ee4ea5fdf9088711a66a18bf4a4c9ee8b24c51d4
throwable <lisitsynavictoria@gmail.com>
2019-02-11 06:25:23 +0500

    Work with the form, implemented adding a new task to the list

--- src/components/TaskAddForm/TaskAddForm.js
+++ src/components/TaskAddForm/TaskAddForm.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './TaskAddForm.css';
 
 export default class TaskAddForm extends Component {
+
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onTaskAdded(this.state.label);
+  }
+
   render() {
     return(
-      <div className="TaskAddForm">
+      <form className="TaskAddForm d-flex" onSubmit={this.onSubmit}>
+      <input type="text" className="form-control" onChange={this.onLabelChange}
+             placeholder="Add a new task" />
+
         <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onTaskAdded('Hi')}>Add Task</button>
-      </div>
+                onClick={() => this.props.onTaskAdded('Hi')}>+</button>
+      </form>
     )
   }
 };
