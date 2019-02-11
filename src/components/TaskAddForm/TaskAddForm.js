07ded9b9cd1403cb3de512f7181dcdfc6c39d2c0
throwable <lisitsynavictoria@gmail.com>
2019-02-11 06:32:14 +0500

    Added form cleansing

--- src/components/TaskAddForm/TaskAddForm.js
+++ src/components/TaskAddForm/TaskAddForm.js
@@ -16,16 +16,17 @@ export default class TaskAddForm extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.props.onTaskAdded(this.state.label);
-  }
+    this.setState({
+      label: ''
+    });
+  };
 
   render() {
     return(
       <form className="TaskAddForm d-flex" onSubmit={this.onSubmit}>
       <input type="text" className="form-control" onChange={this.onLabelChange}
-             placeholder="Add a new task" />
-
-        <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onTaskAdded('Hi')}>+</button>
+             placeholder="Add a new task"  value={this.state.label}/>
+        <button className="btn btn-outline-secondary">+</button>
       </form>
     )
   }
